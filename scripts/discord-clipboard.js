81b92f5091e00bb595c8c9858450196bdf366fdc
Shibzel <117636867+Shibzel@users.noreply.github.com>
2023-08-05 22:22:09 +0200

    First version of the website.

---
+++ scripts/discord-clipboard.js
@@ -0,0 +1,19 @@
+document.getElementById('discord-logo').addEventListener('click', function () {
+    const discordTag = 'shibzel';
+
+    const tempInput = document.createElement('input');
+    tempInput.value = discordTag;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+
+    const popupMessage = document.getElementById('popup-message');
+    const copiedTagElement = document.getElementById('copied-tag');
+    copiedTagElement.textContent = discordTag;
+    popupMessage.style.display = 'block';
+
+    setTimeout(function () {
+        popupMessage.style.display = 'none';
+    }, 7000);
+});
